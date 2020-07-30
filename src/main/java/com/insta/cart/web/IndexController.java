17c23537b503591b6fec7d57adde73d27810c401
da <alrean@naver.com>
2020-07-30 19:54:36 +0900

    스프링부트 프로젝트 - insta cart templates&contoroller 완성

---
+++ src/main/java/com/insta/cart/web/IndexController.java
@@ -0,0 +1,55 @@
+package com.insta.cart.web;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+
+
+    @GetMapping("/index") //@RequestMapping(method=RequestMethod.GET)
+    public String index() {
+        return "/ogani/index";
+    }
+
+    @GetMapping("/blog") //@RequestMapping(method=RequestMethod.GET)
+    public String blog() {
+        return "/ogani/blog";
+    }
+
+    @GetMapping("/checkout") //@RequestMapping(method=RequestMethod.GET)
+    public String checkout() {
+        return "/ogani/checkout";
+    }
+
+    @GetMapping("/contact") //@RequestMapping(method=RequestMethod.GET)
+    public String contact() {
+        return "/ogani/contact";
+    }
+
+    @GetMapping("/main") //@RequestMapping(method=RequestMethod.GET)
+    public String main() {
+        return "/ogani/main";
+    }
+
+    @GetMapping("/shop-details") //@RequestMapping(method=RequestMethod.GET)
+    public String shopDetail() {
+        return "/ogani/shop-details";
+    }
+
+    @GetMapping("/shop-grid") //@RequestMapping(method=RequestMethod.GET)
+    public String shopGrid() {
+        return "/ogani/shop-grid";
+    }
+
+    @GetMapping("/shoping-cart") //@RequestMapping(method=RequestMethod.GET)
+    public String shopingCart() {
+        return "/ogani/shoping-cart";
+    }
+
+
+}
