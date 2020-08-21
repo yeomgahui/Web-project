51111db093be6c1a485763d86d1002504bad4fe7
da <alrean@naver.com>
2020-08-21 15:20:26 +0900

    주문서 분리, 카카오결제 API 구현 0821

--- src/main/java/com/cartrapido/main/service/OrderNumService.java
+++ src/main/java/com/cartrapido/main/service/OrderNumService.java
@@ -4,6 +4,7 @@ import com.cartrapido.main.domain.entity.OrderNum;
 import com.cartrapido.main.domain.repository.OrderNumRepository;
 import com.cartrapido.main.web.dto.OrderNumDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,11 +25,15 @@ public class OrderNumService {
     public List<OrderNumDTO> getOrderNumList() {
         List<OrderNum> orderNumList =orderNumRepository.findAll();
 
+
+
         List<OrderNumDTO> orderNumDTOList =new ArrayList<>();
         for(OrderNum orderNum : orderNumList){
             OrderNumDTO orderNumDTO = new OrderNumDTO(
                     orderNum.getOrderNum(), orderNum.getUserEmail(), orderNum.getShopper(),
-                    orderNum.getLongitude(), orderNum.getLatitud()
+                    orderNum.getLongitude(), orderNum.getLatitud(),
+                    orderNum.getDeliveryCost(), orderNum.getProductTot(),
+                    orderNum.getPay()
             );
 
             orderNumDTOList.add(orderNumDTO);
@@ -42,12 +47,44 @@ public class OrderNumService {
         for (OrderNum orderNum : orderNumList) {
             OrderNumDTO orderNumDTO = new OrderNumDTO(
                     orderNum.getOrderNum(), orderNum.getUserEmail(), orderNum.getShopper(),
-                    orderNum.getLongitude(), orderNum.getLatitud()
+                    orderNum.getLongitude(), orderNum.getLatitud(),
+                    orderNum.getDeliveryCost(), orderNum.getProductTot(),
+                    orderNum.getPay()
             );
             orderNumDTOList.add(orderNumDTO);
         }
         return  orderNumDTOList;
     }
+
+    public List<OrderNumDTO> getPaidOrder(String userEmail, int pay) {
+        List<OrderNum> orderNumList = orderNumRepository.findAllByUserEmailAndPay(userEmail,pay);
+        List<OrderNumDTO> orderNumDTOList = new ArrayList<>();
+        for (OrderNum orderNum : orderNumList) {
+            OrderNumDTO orderNumDTO = new OrderNumDTO(
+                    orderNum.getOrderNum(), orderNum.getUserEmail(), orderNum.getShopper(),
+                    orderNum.getLongitude(), orderNum.getLatitud(),
+                    orderNum.getDeliveryCost(), orderNum.getProductTot(),
+                    orderNum.getPay()
+            );
+            orderNumDTOList.add(orderNumDTO);
+        }
+        return  orderNumDTOList;
+    }
+    public List<OrderNumDTO> shopperGetPaidOrder(int pay) {
+        List<OrderNum> orderNumList = orderNumRepository.findAllByPay(pay);
+        List<OrderNumDTO> orderNumDTOList = new ArrayList<>();
+        for (OrderNum orderNum : orderNumList) {
+            OrderNumDTO orderNumDTO = new OrderNumDTO(
+                    orderNum.getOrderNum(), orderNum.getUserEmail(), orderNum.getShopper(),
+                    orderNum.getLongitude(), orderNum.getLatitud(),
+                    orderNum.getDeliveryCost(), orderNum.getProductTot(),
+                    orderNum.getPay()
+            );
+            orderNumDTOList.add(orderNumDTO);
+        }
+        return  orderNumDTOList;
+    }
+
 
     public void acceptOrder(Long orderNum, String shopperEmail) {
         OrderNum orderNum1 = orderNumRepository.findByOrderNum(orderNum);
@@ -62,11 +99,19 @@ public class OrderNumService {
         for (OrderNum orderNum : orderNumList) {
             OrderNumDTO orderNumDTO = new OrderNumDTO(
                     orderNum.getOrderNum(), orderNum.getUserEmail(), orderNum.getShopper(),
-                    orderNum.getLongitude(), orderNum.getLatitud()
+                    orderNum.getLongitude(), orderNum.getLatitud(),
+                    orderNum.getDeliveryCost(), orderNum.getProductTot(),
+                    orderNum.getPay()
             );
             orderNumDTOList.add(orderNumDTO);
         }
         return  orderNumDTOList;
+    }
+
+    public void updatePay(Long orderNum){
+        OrderNum orderNum1 = orderNumRepository.findByOrderNum(orderNum);
+        orderNum1.setPay(1);
+        orderNumRepository.save(orderNum1);
     }
 
 /*    public OrderNumDTO getOrderNum(OrderNum orderNum){
