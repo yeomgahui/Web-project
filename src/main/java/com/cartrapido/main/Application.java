14076c3978eb8d863a9f1f3cdf4506c386eb29f1
angyeongeun <anruddms8124@gmail.com>
2020-09-23 15:06:17 +0900

    gyoengeun - deploy test03 20.09.23

--- src/main/java/com/cartrapido/main/Application.java
+++ src/main/java/com/cartrapido/main/Application.java
@@ -8,6 +8,7 @@ import org.omg.CORBA.ServerRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
