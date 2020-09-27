8c455020da9864272dd808e4a925f6be28e17d42
angyeongeun <anruddms8124@gmail.com>
2020-09-27 15:33:41 +0900

    gyoengeun - deploy test04 20.09.27

--- src/main/java/com/cartrapido/main/web/IndexController.java
+++ src/main/java/com/cartrapido/main/web/IndexController.java
@@ -19,7 +19,7 @@ public class IndexController {
     private final HttpSession httpSession;
     private final ChatRoomService chatRoomService;
     private final MemberService memberService;
-    //tomcat error fix5
+    //tomcat error fix6
 
 
 
